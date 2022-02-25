e67d212ad5d3cbec6a06267f6626f98b25689d60
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-02-25 10:38:46 +0530

    #include<stdio.h> int main() {     int arr[100],i,n,j,f=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {     	f=0;       for(j=0;j<arr[i];j++)       {           if(j*j==arr[i])           {               f=1;               break;           }       } if(f==1) {     printf("True "); } else {     printf("False "); } } }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,n,j,f=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    	f=0;
+      for(j=0;j<arr[i];j++)
+      {
+          if(j*j==arr[i])
+          {
+              f=1;
+              break;
+          }
+      }
+if(f==1)
+{
+    printf("True
+");
+}
+else
+{
+    printf("False
+");
+}
+}
+}
