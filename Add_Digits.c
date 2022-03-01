9dec0c890eaa52c91a994f3d3ede54d6231b0ed3
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-03-01 11:47:11 +0530

    #include<stdio.h>
    int main()
    {    
        
        int n,sum = 0,rem;
        scanf("%d",&n);   
    
        while(n/10 != 0)
        {
            sum = 0;
            while(n!= 0) 
            {
                rem = n% 10;  
                sum += rem; 
                n = n/ 10;  
            }
    
            n = sum;
        }
    
        printf("%d", sum);
    
        return 0;
    }

---
+++ Add_Digits.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h> 
+
+int main()
+{    
+    
+    int n,sum = 0,rem;
+    scanf("%d",&n);   
+
+    while(n/10 != 0)
+    {
+        sum = 0;
+        while(n!= 0) 
+        {
+            rem = n% 10;  
+            sum += rem; 
+            n = n/ 10;  
+        }
+
+        n = sum;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
