c756f4fa23fc314840a80878baa565886408e1df
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-04-11 19:49:37 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j)             printf("0");             else             printf("x");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            if(i==j)
+            printf("0");
+            else
+            printf("x");
         }
         printf("
 ");
     }
-return 0;
 }
