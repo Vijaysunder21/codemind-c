9d2e0a49a0265a79999db48c42a6da6f2f2763ac
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-04-22 21:03:47 +0530

    #include <stdio.h> int main() {     char str[1000];     int c=0,i;     scanf("%[^ ]s",&str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char str[1000];
+    int c=0,i;
+    scanf("%[^
+]s",&str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
