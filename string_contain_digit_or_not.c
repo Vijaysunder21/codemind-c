eaf1eaec16b20f8e7f12a62dada9cc3b923f5673
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-05-02 11:46:28 +0530

    #include <stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for (i=0;str[i]!=NULL;i++)     {         if ((str[i]>='0')&&(str[i]<='9'))         {             c++;         }     }     if(c==0)     {         printf("Doesn't contain digit",c);     }     else     printf("Contains %d digit",c);     return 0; }

---
+++ string_contain_digit_or_not.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for (i=0;str[i]!=NULL;i++)
+    {
+        if ((str[i]>='0')&&(str[i]<='9'))
+        {
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("Doesn't contain digit",c);
+    }
+    else
+    printf("Contains %d digit",c);
+    return 0;
+}
