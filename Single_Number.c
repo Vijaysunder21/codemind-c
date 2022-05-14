1e5db26d818c391dfb5942e15ea8fb501d5b030d
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-05-14 18:36:40 +0530

    #include<stdio.h> int main() {     int i,j,n,arr[1000000],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)    {        printf("%d",arr[i]);    }     }
    }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,arr[1000000],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+   {
+       printf("%d",arr[i]);
+   }
+    }
+   
+}
